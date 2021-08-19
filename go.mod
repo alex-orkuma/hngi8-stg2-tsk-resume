1eb1b17d80c71a319bb93a48ea3b3c6e9a12c820
alex-orkuma <orkumashi@gmail.com>
2021-08-19 05:39:47 +0100

    opened a new sql connection pool, implemented database interaction

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/alex-orkuma/hngi8-stg2-tsk-resume
 
 go 1.16
+
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+	github.com/go-sql-driver/mysql v1.6.0
+)
--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
+	"time"
+
+	"github.com/bmizerany/pat"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
+var db *sql.DB
 
+type Contact struct {
+	ID      int
+	Name    string
+	Email   string
+	Content string
+	Created time.Time
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./index.page.tmpl",
 	}
@@ -33,19 +44,51 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func createContact(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+	}
+
+	name := r.PostForm.Get("name")
+	email := r.PostForm.Get("email")
+	content := r.PostForm.Get("content")
+
+	_, err = addContact(Contact{
+		Name:    name,
+		Email:   email,
+		Content: content,
+		Created: time.Now(),
+	})
+
+	if err != nil {
+		fmt.Printf("addContact: %v", err)
+	}
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "Network address")
+	dsn := flag.String("dsn", "alex:Or$kumashnd417@/contactbox?parseTime=True", "MYSQL data source name")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
 	erroLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	var err error
+	db, err = openDB(*dsn)
+	if err != nil {
+		erroLog.Fatal(err)
+	}
+
+	defer db.Close()
 	//initialize a new severmux
-	mux := http.NewServeMux()
+	mux := pat.New()
 
 	// register the home handler
-	mux.HandleFunc("/", home)
+	mux.Get("/", http.HandlerFunc(home))
+	mux.Post("/contact", http.HandlerFunc(createContact))
 
 	srv := &http.Server{
 		Addr:     *addr,
@@ -56,6 +99,30 @@ func main() {
 	infoLog.Printf("Starting a server on %s", *addr)
 
 	// start a new webserver on port :4000
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	erroLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func addContact(cont Contact) (int64, error) {
+	result, err := db.Exec("INSERT INTO contacts (name, email, content, created) VALUES( ?, ?, ?, ?)", cont.Name, cont.Email, cont.Content, cont.Created)
+
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
